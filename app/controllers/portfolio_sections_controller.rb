c42c5e75e168cdc6c2993e86c3ad5a6cda596b28
Todd Carlson <todd@s1data.com>
2021-10-07 15:12:46 -0600

    Integrated new and create actions for portfolios

--- app/controllers/portfolio_sections_controller.rb
+++ app/controllers/portfolio_sections_controller.rb
@@ -2,4 +2,20 @@ class PortfolioSectionsController < ApplicationController
     def index
         @portfolio_items = PortfolioSection.all 
     end
-end
+
+    def new 
+        @portfolio_item = PortfolioSection.new
+    end
+
+    def create
+        @portfolio_item = PortfolioSection.new(params.require(:portfolio_section).permit(:title, :subtitle, :body))   
+    
+        respond_to do |format|
+            if @portfolio_item.save
+                format.html { redirect_to portfolio_sections_path, notice: "Your portfolio item is now live." }
+            else
+                format.html { render :new, status: :unprocessable_entity }
+            end
+        end
+    end
+end 

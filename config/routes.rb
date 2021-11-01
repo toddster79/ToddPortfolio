b03785dcf969a9ace09a8a2b754b27676e798ac1
Todd Carlson <todd@s1data.com>
2021-11-01 15:06:39 -0600

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolio_sections, except: [:show]
   get 'angular-items', to: "portfolio_sections#angular"
   get 'portfolio_section/:id', to: 'portfolio_sections#show', as: 'portfolio_section_show'

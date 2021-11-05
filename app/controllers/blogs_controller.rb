362a6fa2f2db16d573f4618d381c9cc8b727e223
Todd Carlson <todd@s1data.com>
2021-11-05 15:09:54 -0600

    Implemented collection based partials

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,7 @@ class BlogsController < ApplicationController
   # GET /blogs or /blogs.json
   def index
     @blogs = Blog.all
-    @page_title = "My Portfolio Blog"
+    @page_title = "My Portfolio Blog"  
   end
 
   # GET /blogs/1 or /blogs/1.json

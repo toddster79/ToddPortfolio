a7879f67d72decc86e6785911a381f8b390b1f56
Todd Carlson <todd@s1data.com>
2021-11-30 15:31:56 -0700

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/portfolio_section.rb
+++ app/models/portfolio_section.rb
@@ -2,9 +2,7 @@ class PortfolioSection < ApplicationRecord
     has_many :technologies
     accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
 
-    # Where we bring in the concern
-    include Placeholder
-    validates_presence_of :title, :body, :main_image, :thumb_image
+    validates_presence_of :title, :body
 
     mount_uploader :thumb_image, PortfolioSectionUploader
     mount_uploader :main_image, PortfolioSectionUploader
@@ -21,11 +19,5 @@ class PortfolioSection < ApplicationRecord
     # Second way to create a custom scope
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
-    # Call to set_defaults method 
-    after_initialize :set_defaults
-    # How to set default values
-    def set_defaults
-        self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-        self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-    end
 end  
+

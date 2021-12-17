25af98f5cb3e1f804e05d0cf6cea9ab591b12b9f
Todd Carlson <todd@s1data.com>
2021-12-17 14:34:34 -0700

    Updated styles for tech news section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,7 +44,7 @@ module ApplicationHelper
             },
             {
                 url: tech_news_path,
-                title: 'Tech News'
+                title: 'Tech Tweets'
             }
         ]
     end

6888fbc97cde259fae9a238136bf14eacb2fc80a
Todd Carlson <todd@s1data.com>
2021-12-22 15:46:21 -0700

    Updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ Bundler.require(*Rails.groups)
 module Portfolio
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
 
